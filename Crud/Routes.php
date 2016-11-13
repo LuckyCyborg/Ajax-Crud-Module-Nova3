923736e7075404d1dbff794a9f1fa3abfa67e825
David Carr <dave@daveismyname.com>
2016-11-13 17:54:09 +0000

    Ensure load is only loaded via ajax.

--- Crud/Routes.php
+++ Crud/Routes.php
@@ -2,7 +2,7 @@
 Route::group(['prefix' => 'admin', 'namespace' => 'App\Modules\Crud\Controllers'], function(){
 	
 	Route::get('crud',               ['before' => 'auth', 'uses' => 'Crud@index']);
-	Route::get('crud/load',          ['before' => 'auth', 'uses' => 'Crud@loadRecords']);
+	Route::get('crud/load',          ['before' => 'auth|ajax', 'uses' => 'Crud@loadRecords']);
 	
 	Route::post('crud',              ['before' => 'auth|ajax', 'uses' => 'Ajax@store']);
 	Route::post('crud/{id}/edit',    ['before' => 'auth|ajax', 'uses' => 'Ajax@edit']);
